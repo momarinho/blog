6cce0c9aa3d58d84a1dba607a38cbbb7cab716d5
momarinho <117489486+momarinho@users.noreply.github.com>
2023-03-01 11:58:19 -0300

    Google auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { auth } from '../config/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Header = () => {
-  const [user] = useAuthState(null);
+  const [user] = useAuthState(auth);
 
   const handleLogin = async () => {
     const provider = new GoogleAuthProvider();
@@ -25,21 +25,20 @@ const Header = () => {
     <nav className="bg-white py-4 px-8 shadow">
       <div className="max-w-7xl mx-auto flex justify-between">
         <div className="font-bold text-xl">My Blog</div>
-        {user && (
-          <>
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-              onClick={handleLogout}
-            >
-              Logout
-            </button>
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-              onClick={handleLogin}
-            >
-              Login with Google
-            </button>
-          </>
+        {user ? (
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        ) : (
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            onClick={handleLogin}
+          >
+            Login with Google
+          </button>
         )}
       </div>
     </nav>
