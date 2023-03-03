753c2064ff9fa3f14db6fed0ee40dbc1fc1a9147
momarinho <117489486+momarinho@users.noreply.github.com>
2023-03-02 21:02:56 -0300

    SearchBar/CSS changes in RecentPosts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '../config/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -6,6 +6,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [user] = useAuthState(auth);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
+  const searchInputRef = useRef(null);
+
+  const handleSearch = () => {
+    console.log(`Searching for "${searchTerm}"...`);
+    //implement 
+  };
 
   const handleLogin = async () => {
     const provider = new GoogleAuthProvider();
@@ -22,35 +30,89 @@ const Header = () => {
     await auth.signOut();
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        searchInputRef.current &&
+        !searchInputRef.current.contains(event.target)
+      ) {
+        setShowSearch(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [searchInputRef]);
+
   return (
     <nav className="bg-white py-4 px-8 shadow-sm">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <Link to="/" className="font-bold text-xl text-gray-800">
           My Blog
         </Link>
-        {user ? (
-          <div className="flex items-center">
-            <img
-              src={user.photoURL}
-              alt="user profile"
-              className="h-8 w-8 rounded-full mr-2"
-            />
-            <div className="text-gray-500 mr-2">{user.displayName}</div>
+        <div className="flex items-center">
+          <div className="relative">
             <button
-              className="text-red-500 hover:text-red-600  font-bold py-2 px-4 rounded"
-              onClick={handleLogout}
+              className="text-blue-500 hover:text-blue-600 focus:outline-none"
+              onClick={() => setShowSearch(true)}
             >
-              Logout
+              Search
             </button>
+            {showSearch && (
+              <div className="absolute top-0 right-0 z-10 bg-white border border-gray-400 rounded w-96">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="w-full border-none py-2 px-4"
+                  ref={searchInputRef}
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button
+                  className="absolute top-0 right-0 px-4 py-2"
+                  onClick={handleSearch}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="#000"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M13.4B8r3B4p7yhRXuBWLqsQ546WR43cqQwrbXMDFnBi6vSJBeif8tPW85a7r7DM961Jvk4hdryZoByEp8GC8HzsqJpRN4FxGM906-1.06l.059-.06a5.482 5.482 0 00-1.844-9.972 5.5 5.5 0 107.783 7.783zM10.5 13a3.5 3.5 0 110-7 3.5 3.5 0 010 7z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+            )}
           </div>
-        ) : (
-          <button
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-            onClick={handleLogin}
-          >
-            Login
-          </button>
-        )}
+
+          {user ? (
+            <>
+              <button
+                className="text-red-500 hover:text-red-600 ml-2"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+              <img
+                src={user.photoURL}
+                alt="user profile"
+                className="h-8 w-8 rounded-full mr-2 ml-2"
+              />
+            </>
+          ) : (
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+              onClick={handleLogin}
+            >
+              Login with Google
+            </button>
+          )}
+        </div>
       </div>
     </nav>
   );
--- src/components/RecentPosts.js
+++ src/components/RecentPosts.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const RecentPosts = ({ posts }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const sortedPosts = posts
     .slice()
     .sort((a, b) => b.createdAt.toDate() - a.createdAt.toDate())
     .slice(0, 5);
-
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -17,55 +17,42 @@ const RecentPosts = ({ posts }) => {
     return () => clearInterval(interval);
   }, [currentIndex, sortedPosts.length]);
 
-  const goToNext = () => {
-    setCurrentIndex((currentIndex + 1) % sortedPosts.length);
-  };
-
-  const goToPrevious = () => {
-    setCurrentIndex(
-      (currentIndex - 1 + sortedPosts.length) % sortedPosts.length
-    );
+  const goToIndex = (index) => {
+    setCurrentIndex(index);
   };
 
   return (
-    <section className="mb-8 ">
+    <section className="mb-8">
       <div className="flex flex-col md:flex-row md:gap-4">
-        {sortedPosts
-          .slice(currentIndex, currentIndex + 3)
-          .map((post, index) => (
-            <div
-              key={post.id}
-              className={`bg-white rounded-lg shadow-md p-24 flex-1 mb-4 md:mb-0 hover:scale-105 ${
-                index > 0 ? 'hidden' : ''
-              }`}
-            >
-              <Link
-                to={`/posts/${post.id}`}
-                className="text-2xl font-bold mb-2"
-              >
-                {post.title}
-              </Link>
-              <div className="flex justify-between items-center ">
-                <p className="text-sm text-gray-500">
-                  {new Date(post.createdAt.toDate()).toLocaleString()}
-                </p>
-              </div>
-              <div className="flex justify-center">
-                <button
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full mr-4"
-                  onClick={goToPrevious}
-                >
-                  &lt;
-                </button>
-                <button
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full"
-                  onClick={goToNext}
-                >
-                  &gt;
-                </button>
+        {sortedPosts.map((post, index) => (
+          <div
+            key={post.id}
+            className={`bg-white rounded-lg shadow-md p-24 flex-1 mb-4 hover:scale-105 ${
+              index === currentIndex ? '' : 'hidden'
+            }`}
+          >
+            <Link to={`/posts/${post.id}`} className="text-xl font-bold mb-2">
+              {post.title}
+            </Link>
+            <p className="text-sm text-gray-500 mb-4">{post.excerpt}</p>
+            <div className="flex justify-between items-center">
+              <p className="text-sm text-gray-500">
+                {new Date(post.createdAt.toDate()).toLocaleString()}
+              </p>
+              <div className="flex">
+                {sortedPosts.map((post, index) => (
+                  <button
+                    key={index}
+                    className={`h-2 w-2 rounded-full mx-1 ${
+                      index === currentIndex ? 'bg-blue-500' : 'bg-gray-300'
+                    }`}
+                    onClick={() => goToIndex(index)}
+                  ></button>
+                ))}
               </div>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </section>
   );
